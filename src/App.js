b0be6df3f16f18cc6bc868e0f7123e7d395149c3
Ayodele Afolabi <105075157+deleayo535@users.noreply.github.com>
2022-08-29 18:16:08 +0100

    file uploader

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, Form } from "antd";
+import Editor from "./components/Editor";
+import Upload from "./components/Uploader";
 
 function App() {
+  // const onSubmit = (event) => {
+  //   event.preventDefault();
+
+  //   setContent("");
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Editor />
+        <Upload />
+      </div>
+      <Form>
+        <div style={{ textAlign: "center", margin: "2rem" }}>
+          <Button size="large" htmlType="submit">
+            Submit
+          </Button>
+        </div>
+      </Form>
+    </>
   );
 }
 
