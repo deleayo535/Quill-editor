59ce9ef2dd0413c85770e26066b70ac3b3972a0c
Ayodele Afolabi <105075157+deleayo535@users.noreply.github.com>
2022-09-02 09:26:48 +0100

    quill edits

---
+++ src/components/Quill.js
@@ -0,0 +1,80 @@
+import styled from "@emotion/styled";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+export function QuillEdit() {
+  const modules = {
+    toolbar: [
+      // [{ header: [1, 2, false] }],
+      [{ font: [] }],
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      [{ script: "sub" }, { script: "super" }], // superscript/subscript
+      [{ direction: "rtl" }], // text direction
+      [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+      [{ align: [] }],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "font",
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+  // const [] = useState();
+  return (
+    <div>
+      <EditorContainer>
+        <input placeholder="Title" />
+        <ReactQuill
+          theme="snow"
+          modules={modules}
+          formats={formats}
+          // readOnly="true"
+          placeholder="Type Something"
+        />
+      </EditorContainer>
+    </div>
+  );
+}
+
+const EditorContainer = styled.div`
+  width: 100%;
+  padding-left: 20%;
+   
+  > input {
+    border: none;
+    outline: none;
+    padding: 18px;
+    font-size: 2em;
+    width: 100%;
+  }
+
+  .ql-toolbar,
+  .ql-container {
+    border: none !important;
+  }
+
+  .quill, .ql-container [
+    font-size: 1em;
+    height: 100%
+  ]
+`;
